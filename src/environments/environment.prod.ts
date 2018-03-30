d5652188af69a48ac7c681a7613b38d81846a681
Shahid Rogers <shahid@flexiroam.org>
2018-03-30 13:53:46 +0800

    heroku deploy: solve postinstall issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCMFXU26fdFlElx1k_0GHKMpvt6PwzO3Y8",
+    authDomain: "todoer-5ba61.firebaseapp.com",
+    databaseURL: "https://todoer-5ba61.firebaseio.com",
+    projectId: "todoer-5ba61",
+    storageBucket: "todoer-5ba61.appspot.com",
+    messagingSenderId: "774178966443"
+  }
 };
